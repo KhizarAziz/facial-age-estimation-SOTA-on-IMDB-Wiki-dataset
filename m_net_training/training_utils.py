8c47ec9847da6f83eeaa0105ad22402dc37ae457
khizarziz <khizer.awan@gmail.com>
2020-12-18 22:29:41 +0000

    bugs fixed

--- m_net_training/training_utils.py
+++ m_net_training/training_utils.py
@@ -133,9 +133,9 @@ def image_transform(row,dropout,target_img_shape,random_erasing=False,random_enf
     # img -= global_mean
 
   #STANDARDIZATION (preferred)
-  means = img.mean(axis=(0,1), dtype='float64') # mean of separate channels
-  stds = img.std(axis=(0,1), dtype='float64') # std of separate channels
-  img = (img - means) / stds
+  # means = img.mean(axis=(0,1), dtype='float64') # mean of separate channels
+  # stds = img.std(axis=(0,1), dtype='float64') # std of separate channels
+  # img = (img - means) / stds
 
   # get trible box (out,middle,inner) and crop image from these boxes then
   face_lm = pickle.loads(row['landmarks'],encoding="bytes")
@@ -189,7 +189,7 @@ def image_transform(row,dropout,target_img_shape,random_erasing=False,random_enf
   # f.savefig("/content/his_gen/{}-{}.jpg".format(row['age'],rr))
 
   cascad_imgs = np.array(tripple_cropped_imgs)
-  if random_erasing:	
+  if random_enforcing:	
       flag = random.randint(0, 3)	
       contrast = random.uniform(0.5, 2.5)	
       bright = random.uniform(-50, 50)	
@@ -418,6 +418,168 @@ class CyclicLR(Callback):
             self.history.setdefault(k, []).append(v)
         
         K.set_value(self.model.optimizer.lr, self.clr())
+
+
+
+#------------------- LR_FINDER ----------------------------
+
+
+# import the necessary packages
+from tensorflow.keras.callbacks import LambdaCallback
+from tensorflow.keras import backend as K
+import matplotlib.pyplot as plt
+import numpy as np
+import tempfile
+class LearningRateFinder:
+	def __init__(self, model, stopFactor=4, beta=0.98):
+		# store the model, stop factor, and beta value (for computing
+		# a smoothed, average loss)
+		self.model = model
+		self.stopFactor = stopFactor
+		self.beta = beta
+		# initialize our list of learning rates and losses,
+		# respectively
+		self.lrs = []
+		self.losses = []
+		# initialize our learning rate multiplier, average loss, best
+		# loss found thus far, current batch number, and weights file
+		self.lrMult = 1
+		self.avgLoss = 0
+		self.bestLoss = 1e9
+		self.batchNum = 0
+		self.weightsFile = None
+
+	def reset(self):
+		# re-initialize all variables from our constructor
+		self.lrs = []
+		self.losses = []
+		self.lrMult = 1
+		self.avgLoss = 0
+		self.bestLoss = 1e9
+		self.batchNum = 0
+		self.weightsFile = None
+
+
+	def is_data_iter(self, data):
+		# define the set of class types we will check for
+		iterClasses = ["NumpyArrayIterator", "DirectoryIterator",
+			 "DataFrameIterator", "Iterator", "Sequence","ImgAgeGenrator"]
+		# return whether our data is an iterator
+		return data.__class__.__name__ in iterClasses
+
+
+	def on_batch_end(self, batch, logs):
+		# grab the current learning rate and add log it to the list of
+		# learning rates that we've tried
+		lr = K.get_value(self.model.optimizer.lr)
+		self.lrs.append(lr)
+		# grab the loss at the end of this batch, increment the total
+		# number of batches processed, compute the average average
+		# loss, smooth it, and update the losses list with the
+		# smoothed value
+		l = logs["loss"]
+		self.batchNum += 1
+		self.avgLoss = (self.beta * self.avgLoss) + ((1 - self.beta) * l)
+		smooth = self.avgLoss / (1 - (self.beta ** self.batchNum))
+		self.losses.append(smooth)
+		# compute the maximum loss stopping factor value
+		stopLoss = self.stopFactor * self.bestLoss
+		# check to see whether the loss has grown too large
+		if self.batchNum > 1 and smooth > stopLoss:
+			# stop returning and return from the method
+			self.model.stop_training = True
+			return
+		# check to see if the best loss should be updated
+		if self.batchNum == 1 or smooth < self.bestLoss:
+			self.bestLoss = smooth
+		# increase the learning rate
+		lr *= self.lrMult
+		K.set_value(self.model.optimizer.lr, lr)
+
+
+
+	def find(self, trainData, startLR, endLR, epochs=None,
+		stepsPerEpoch=None, batchSize=32, sampleSize=2048,
+		verbose=1):
+		# reset our class-specific variables
+		self.reset()
+		# determine if we are using a data generator or not
+		useGen = True# self.is_data_iter(trainData)
+		# if we're using a generator and the steps per epoch is not
+		# supplied, raise an error
+		if useGen and stepsPerEpoch is None:
+			msg = "Using generator without supplying stepsPerEpoch"
+			raise Exception(msg)
+		# if we're not using a generator then our entire dataset must
+		# already be in memory
+		elif not useGen:
+			# grab the number of samples in the training data and
+			# then derive the number of steps per epoch
+			numSamples = len(trainData[0])
+			stepsPerEpoch = np.ceil(numSamples / float(batchSize))
+		# if no number of training epochs are supplied, compute the
+		# training epochs based on a default sample size
+		if epochs is None:
+			epochs = int(np.ceil(sampleSize / float(stepsPerEpoch)))
+
+		# compute the total number of batch updates that will take
+		# place while we are attempting to find a good starting
+		# learning rate
+		numBatchUpdates = epochs * stepsPerEpoch
+		# derive the learning rate multiplier based on the ending
+		# learning rate, starting learning rate, and total number of
+		# batch updates
+		self.lrMult = (endLR / startLR) ** (1.0 / numBatchUpdates)
+		# create a temporary file path for the model weights and
+		# then save the weights (so we can reset the weights when we
+		# are done)
+		self.weightsFile = tempfile.mkstemp()[1]
+		self.model.save_weights(self.weightsFile)
+		# grab the *original* learning rate (so we can reset it
+		# later), and then set the *starting* learning rate
+		origLR = K.get_value(self.model.optimizer.learning_rate)
+		K.set_value(self.model.optimizer.learning_rate, startLR)
+
+		# construct a callback that will be called at the end of each
+		# batch, enabling us to increase our learning rate as training
+		# progresses
+		callback = LambdaCallback(on_batch_end=lambda batch, logs:
+			self.on_batch_end(batch, logs))
+		# check to see if we are using a data iterator
+		if useGen:
+			self.model.fit(
+				x=trainData,
+				steps_per_epoch=stepsPerEpoch,
+				epochs=epochs,
+				verbose=verbose,
+				callbacks=[callback])
+		# otherwise, our entire training data is already in memory
+		else:
+			# train our model using Keras' fit method
+			self.model.fit(
+				x=trainData[0], y=trainData[1],
+				batch_size=batchSize,
+				epochs=epochs,
+				callbacks=[callback],
+				verbose=verbose)
+		# restore the original model weights and learning rate
+		self.model.load_weights(self.weightsFile)
+		K.set_value(self.model.optimizer.lr, origLR)
+
+	def plot_loss(self, skipBegin=10, skipEnd=1, title=""):
+		# grab the learning rate and losses values to plot
+		lrs = self.lrs[skipBegin:-skipEnd]
+		losses = self.losses[skipBegin:-skipEnd]
+		# plot the learning rate vs. loss
+		plt.plot(lrs, losses)
+		plt.xscale("log")
+		plt.xlabel("Learning Rate (Log Scale)")
+		plt.ylabel("Loss")
+    # if the title is not empty, add it to the plot
+		if title != "":
+			plt.title(title)
+		plt.savefig('/content/fig.jpg')
+
 
 
 
